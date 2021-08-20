4ea6253dd07c2413016a3f74d333c98d8ef4e2d0
praiseopara <cpopara2010@yahoo.com>
2021-08-20 09:44:44 -0500

    created the login, signup forms and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,16 +29,10 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
-    // res.render('homepage', {
-    //     id: 1,
-    //     post_url: 'https://handlebarsjs.com/guide/',
-    //     title: 'handlebars docs',
-    //     created_at: new Date(),
-    //     comments: [{}, {}],
-    //     user: {
-    //         username: 'test_user'
-    //     }
-    // });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
 });
 
 module.exports = router;
