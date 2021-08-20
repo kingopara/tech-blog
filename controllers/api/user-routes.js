2bd346848f3e358e3f688450379a2099179d52f6
praiseopara <cpopara2010@yahoo.com>
2021-08-20 10:33:07 -0500

    created logout route and express sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -80,8 +88,26 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect Password!' });
             return;
         }
-        res.json({ user: dbUserData, message: "Logged In!" });
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: "You are now Logged In!" });
+        });
     });
+});
+
+//logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 // put route for /api/users/1
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,6 +32,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
     res.render('login')
 });
 
