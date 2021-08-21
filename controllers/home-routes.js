b190fbdef9d49ad17259158dd84f4b4b824bf40d
praiseopara <cpopara2010@yahoo.com>
2021-08-20 20:54:39 -0500

    fixing singlepost page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,8 +36,44 @@ router.get('/login', (req, res) => {
         res.redirect('/');
         return;
     }
-    
+
     res.render('login')
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'no post found' });
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', { post });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
