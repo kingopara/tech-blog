da2fe77cd0b33e3eee8d3667698918f8678d47c3
praiseopara <cpopara2010@yahoo.com>
2021-08-21 22:49:26 -0500

    everything works fine

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,7 +5,7 @@ const withAuth = require('../../utils/auth');
 router.get('/', (req, res) => {
     console.log('====================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'content', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'content', 'title', 'created_at'],
         include: [
             {
                 model: Comment,
@@ -67,7 +67,7 @@ router.get('/:id', (req, res) => {
 router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
-        post_url: req.body.post_url,
+        content: req.body.content,
         user_id: req.session.user_id
     })
     .then(dbPostData => res.json(dbPostData))
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id','post_url', 'title', 'created_at'],
+        attributes: ['id','content', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -48,7 +48,7 @@ router.get('/post/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'content', 'title', 'created_at'],
         include: [
             {
                 model: Comment,
